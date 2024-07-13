27e95cfe0dd75765be9c277f26c2952420cce599
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-07-13 13:41:46 +0530

    product view completed

--- app/screens/ViewProduct/index.tsx
+++ app/screens/ViewProduct/index.tsx
@@ -1,20 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { View, ScrollView, StyleSheet, Image, Dimensions, Text } from 'react-native';
-import { useIsFocused } from '@react-navigation/native';
+import { View, ScrollView, StyleSheet, Image, Dimensions, Text, TouchableOpacity } from 'react-native';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
 import { SwiperFlatList } from 'react-native-swiper-flatlist';
 import * as Animatable from 'react-native-animatable';
 import { StatusBar } from 'expo-status-bar';
-import { IconButton } from 'react-native-paper';
+import { Icon, IconButton } from 'react-native-paper';
 import { StarRatingDisplay } from 'react-native-star-rating-widget';
+import { LinearGradient } from 'expo-linear-gradient';
+import Spinner from 'react-native-loading-spinner-overlay';
+
+
+const color = ["#090979", "#433eb6", "#433eb6"];
 const { width, height } = Dimensions.get('window');
 
 const ViewProduct = (props) => {
   const { productInfo } = props.route.params;
   const isFocused = useIsFocused();
 
+  const getFutureDate = (daysAhead) => {
+    const today = new Date();
+    today.setDate(today.getDate() + daysAhead);
+    const options = { weekday: 'long', month: 'long', day: 'numeric' };
+    return today.toLocaleDateString('en-US', options);
+  };
   // Force re-render when screen is focused
   const [key, setKey] = useState(0);
-  
+
   useEffect(() => {
     if (isFocused) {
       setKey(prevKey => prevKey + 1);
@@ -22,10 +33,10 @@ const ViewProduct = (props) => {
   }, [isFocused]);
 
   return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+    <ScrollView showsVerticalScrollIndicator={false} style={styles.container} contentContainerStyle={styles.contentContainer}>
       <StatusBar backgroundColor='transparent' style={'auto'} />
       <ProductImageSwiper key={key} item={productInfo.images} />
-      <Text style={styles.titleText}>{productInfo.title.split(' ').slice(0,3).join(' ')}</Text>
+      <Text style={styles.titleText}>{productInfo.title.split(' ').slice(0, 3).join(' ')}</Text>
       <View style={styles.ratingContainer}>
         <StarRatingDisplay
           rating={3.5}
@@ -35,36 +46,71 @@ const ViewProduct = (props) => {
           color="#FFA400"
 
         />
-        <Text style={{fontSize:width * 0.04, fontWeight:'600'}}>3.5</Text>
+        <Text style={{ fontSize: width * 0.04, fontWeight: '600' }}>3.5</Text>
       </View>
       <View style={styles.priceContainer}>
-      <Text style={{fontSize:width * 0.05, fontWeight:'500'}} >{"₹" + productInfo.price + "99"}</Text>
-      <Text style={{fontSize:width * 0.035, opacity:0.5}}>exclucing delivery charges</Text>
+        <Text style={{ fontSize: width * 0.05, fontWeight: '500' }} >{"₹" + productInfo.price + "99"}</Text>
+        <Text style={{ fontSize: width * 0.035, opacity: 0.5 }}>exclucing delivery charges</Text>
       </View>
       <View style={styles.descriptionContainer}>
-      <Text textBreakStrategy={'simple'} style={styles.descriptionText}>{productInfo.description}</Text>
+        <Text textBreakStrategy={'simple'} style={styles.descriptionText}>{productInfo.description}</Text>
       </View>
-      
+      <View style={styles.deliveryTextContainer}>
+        <Icon
+          source="truck-fast-outline"
+          color={'#433eb6'}
+          size={width * 0.08}
+        />
+        <Text style={{ fontSize: width * 0.037, fontWeight: '500' }}>Delivery by {getFutureDate(5)}</Text>
+      </View>
+      <TouchableOpacity  style={{ elevation: 5, left: width * 0.06, }}>
+        <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} colors={color} style={styles.btn}>
+          <Icon
+            source="cart-outline"
+            color={'#F7F1F1'}
+            size={width * 0.06}
+          />
+          <Text style={{
+            fontSize: width * 0.04,
+            color: 'white'
+          }} >Add to cart</Text>
+        </LinearGradient>
+      </TouchableOpacity>
+      {/* <View style={styles.priceContainer}>
+      <Icon
+          source="sticker-check-outline"
+          color={'#433eb6'}
+          size={width * 0.08}
+        />
+        <Text style={{ fontSize: width * 0.04, opacity: 0.7 }}>item added to cart</Text>
+      </View> */}
+      {/* <Spinner
+        visible={true}
+        color="#090979"
+        size={50}
+      /> */}
+
     </ScrollView>
   );
 }
 
 const ProductImageSwiper = ({ item }) => {
+  const navigation = useNavigation();
   return (
-    <Animatable.View animation={'fadeInDown'} duration={1500} style={styles.pageContainer}>
+    <Animatable.View animation={'fadeInDown'} duration={1000} style={styles.pageContainer}>
       <View style={styles.iconContainer}>
         <IconButton
           icon="keyboard-backspace"
           iconColor='#433eb6'
           size={30}
-          style={{backgroundColor:'#E7E5DF'}}
-          onPress={() => console.log('Pressed')}
+          style={{ backgroundColor: '#E7E5DF' }}
+          onPress={() => navigation.goBack()}
         />
         <IconButton
           icon="heart"
           iconColor='#433eb6'
           size={30}
-        style={{backgroundColor:'#E7E5DF'}}
+          style={{ backgroundColor: '#E7E5DF' }}
           onPress={() => console.log('Pressed')}
         />
       </View>
@@ -94,8 +140,9 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
 
-    paddingBottom: height * 0.2,
+    paddingBottom: height * 0.03,
     justifyContent: 'center',
+
   },
   pageContainer: {
     width: width,
@@ -147,9 +194,9 @@ const styles = StyleSheet.create({
   },
   ratingContainer: {
     width: width * 0.45,
-    flexDirection:'row',
+    flexDirection: 'row',
     height: 35,
-    gap:10,
+    gap: 10,
     left: width * 0.06,
     marginTop: height * 0.01,
     justifyContent: 'center',
@@ -158,37 +205,55 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderRadius: 10
   },
-  priceContainer:{
+  priceContainer: {
     marginTop: height * 0.02,
-    width:width*0.8,
-    height:height*0.065,
-    justifyContent:'center',
-    alignItems:'center',
-    flexDirection:'row',
-    borderColor:'black',
-    backgroundColor:'#E7E5DF',
+    width: width * 0.8,
+    height: height * 0.065,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderColor: 'black',
+    backgroundColor: '#E7E5DF',
     left: width * 0.06,
-    borderRadius:10,
-    elevation:1,
-    gap:width*0.05
+    borderRadius: 10,
+    elevation: 1,
+    gap: width * 0.05
   },
- descriptionContainer:{
-     width:width*0.9,
-     justifyContent:'center',
-     alignItems:'center',
-     
-     borderColor:'black',
-     
-     marginTop: height * 0.02,
-     left: width * 0.06
- },
-  descriptionText:{
-    fontSize:width*0.037,
-   
-    
-   
-    
+  descriptionContainer: {
+    width: width * 0.9,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    borderColor: 'black',
+
+    marginTop: height * 0.02,
+    left: width * 0.06
+  },
+  descriptionText: {
+    fontSize: width * 0.037,
+  },
+  deliveryTextContainer: {
+    flexDirection: 'row',
+
+    borderColor: 'black',
+    width: width * 0.6,
+    left: width * 0.06,
+    marginTop: height * 0.016,
+    height: height * 0.065,
+    gap: 5,
+    alignItems: 'center'
+
+  },
+  btn: {
+    width: width * 0.53,
+    height: width * 0.13,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: 7
   }
+
 });
 
 export default ViewProduct;
