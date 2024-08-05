28fa0dc9b6cae6f049783990b306450638943d9a
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-08-05 13:46:54 +0530

    added billing details

--- app/screens/ViewOrder/index.tsx
+++ app/screens/ViewOrder/index.tsx
@@ -1,16 +1,18 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { View, Text, StyleSheet, Dimensions, ScrollView, Button, Image } from "react-native";
 import { IconButton } from "react-native-paper";
+import StepIndicator from 'react-native-step-indicator';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 const { width, height } = Dimensions.get('window');
 const color = ["#090979", "#433eb6", "#433eb6"];
-import StepIndicator from 'react-native-step-indicator';
+
 const ViewOrder = ({ navigation, route }) => {
     const { orderDetails } = route.params;
     // console.log("<<<<", orderDetails);
     const checkConfirm: boolean = orderDetails.paymentData.paymentId != '' && orderDetails.paymentData.paymentStatus === 'Pending';
+    const subTotal = orderDetails.products.reduce((sum, item) => sum + item.price * item.quantity, 0)
 
-
-    const labels = [checkConfirm ? 'not confirmed ❌' : 'Confirmed', 'Processing', 'Shipped', 'On the way', 'Delivered'];
+    const labels = [checkConfirm ? 'not confirmed ❌' : 'Confirmed', 'Processing', 'Shipped', 'In Transit', 'Delivered'];
     const customStyles = {
         stepIndicatorSize: width * 0.06,
         currentStepIndicatorSize: width * 0.07,
@@ -75,11 +77,77 @@ const ViewOrder = ({ navigation, route }) => {
                 direction={'vertical'}
             />
         </View>
+        <Text style={{ fontSize: width * 0.042, fontFamily: 'RobotoSlab_semiBold', color: '#090979', alignSelf: 'flex-start', marginBottom: 5, marginLeft: width * 0.1 }}>{'all item(s)'}</Text>
         <View style={styles.productsContainer}>
-            <ScrollView showsHorizontalScrollIndicator ={false} horizontal={true}>
+            <ScrollView showsHorizontalScrollIndicator={false} horizontal={true}>
                 {orderDetails.products.map((item, index) => <Products item={item} key={index} />)}
 
             </ScrollView>
+        </View>
+        <Text style={{ fontSize: width * 0.042, fontFamily: 'RobotoSlab_semiBold', color: '#090979', alignSelf: 'flex-start', marginBottom: 5, marginLeft: width * 0.1 }}>{'billing details'}</Text>
+
+        {/* billing details conatiner */}
+        <View style={styles.billingContainer}>
+           {orderDetails.paymentMode != 'Cash on delivery'?
+            <View style={[styles.paymentStatus, {backgroundColor: checkConfirm?'#E9374F':'#2FBF71'}]} >
+            
+            <Text style={{ fontSize: width * 0.048, fontFamily: 'RobotoSlab_semiBold', color:'#E7E5DF' }} >{checkConfirm?'Payment failed':'Paid'}</Text>
+            <Icon name= {checkConfirm?'error-outline':'done'}  size={width*0.07} color={"#E7E5DF"} />
+            </View>:null }
+            {/* name details */}
+            <View style={{ width: width * 0.8, height: height * 0.06, borderColor: 'black', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_semiBold' }} >Name</Text>
+                <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_regular' }} > {orderDetails.name}</Text>
+
+            </View>
+
+            {/* phone details */}
+
+            <View style={{ width: width * 0.8, height: height * 0.06, borderColor: 'black', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_semiBold' }} >Phone</Text>
+                <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_regular' }} >{"+91" + orderDetails.phone}</Text>
+            </View>
+            {/* email details  */}
+            <View style={{ width: width * 0.8, height: height * 0.06, borderColor: 'black', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_semiBold' }} >Email</Text>
+                <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_regular' }} >{orderDetails.email}</Text>
+            </View>
+
+            {/* Shipping details */}
+            <View style={{ width: width * 0.8, borderColor: 'black', alignItems: 'flex-start', }}>
+                <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_semiBold' }} >Shipping Address</Text>
+                <Text style={{ fontSize: width * 0.037, fontFamily: 'RobotoSlab_regular', textAlign: 'justify', }} >{orderDetails.address}</Text>
+            </View>
+            {/* payment mode details */}
+            <View style={{ width: width * 0.8, height: height * 0.06, borderColor: 'black', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_semiBold' }} >Payment mode : </Text>
+                <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_regular' }} >{orderDetails.paymentMode}</Text>
+            </View>
+            {orderDetails.paymentMode != 'Cash on delivery' ?
+                <View style={{ width: width * 0.8, height: height * 0.06, borderColor: 'black', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_semiBold' }} >Payment ID : </Text>
+                    <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_regular' }} >{orderDetails.paymentData.paymentId}</Text>
+                </View> : null
+
+            }
+            <View style={{ width: width * 0.8, height: height * 0.06, borderColor: 'black', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_semiBold' }} >{`Sub total (${orderDetails.products.reduce((sum, item) => sum + item.quantity, 0)} items)`}</Text>
+                <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_semiBold' }} > {"₹" + subTotal}</Text>
+
+            </View>
+            <View style={{ width: width * 0.8, height: height * 0.06, borderColor: 'black', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_semiBold' }} >{"delivery charge"}</Text>
+                <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_semiBold' }} >{"₹ " + "99"}</Text>
+            </View>
+            <View style={styles.divider}>
+                {/*  divider      */}
+            </View>
+            <View style={{ width: width * 0.8, height: height * 0.07, borderColor: 'black', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                <Text style={{ fontSize: width * 0.05, fontFamily: 'RobotoSlab_semiBold' }} >{"Total"}</Text>
+                <Text style={{ fontSize: width * 0.05, fontFamily: 'RobotoSlab_semiBold' }} >{"₹ " + (subTotal + 99)}</Text>
+            </View>
+
+
         </View>
     </ScrollView>)
 }
@@ -89,18 +157,18 @@ const Products = ({ item }) => {
 
     return (<View style={styles.products}>
 
-        <Image resizeMode={'cover'} style={{ width: width * 0.2, height: width * 0.25 , marginLeft:5, borderRadius:10}} source={{ uri: item.productId.images[0] }} />
+        <Image resizeMode={'cover'} style={{ width: width * 0.2, height: width * 0.25, marginLeft: 5, borderRadius: 10 }} source={{ uri: item.productId.images[0] }} />
         <View style={styles.productInfoContainer}>
-            <Text style={{fontSize:width*0.04, fontFamily: 'RobotoSlab_regular'}}>
-                {item.productId.description.length > 30
-                    ? item.productId.description.substring(0, 30) + '...'
-                    : item.productId.description}
+            <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_regular' }}>
+                {item.productId.title.length > 30
+                    ? item.productId.title.substring(0, 30) + '...'
+                    : item.productId.title}
             </Text>
-            <Text style={{fontSize:width*0.04, fontFamily: 'RobotoSlab_semiBold'}}>
-            Quantity {item.quantity}
+            <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_semiBold' }}>
+                Quantity {item.quantity}
             </Text>
-            <Text style={{fontSize:width*0.04, fontFamily: 'RobotoSlab_semiBold'}}>
-            {'₹ ' + item.productId.price}
+            <Text style={{ fontSize: width * 0.04, fontFamily: 'RobotoSlab_semiBold' }}>
+                {'₹ ' + item.productId.price}
             </Text>
 
         </View>
@@ -147,10 +215,10 @@ const styles = StyleSheet.create({
     productsContainer: {
         width: width * 0.9,
         height: height * 0.17,
-       
+        // borderWidth:1,
         borderColor: 'black',
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'flex-start',
         alignSelf: "center"
     },
     products: {
@@ -158,6 +226,7 @@ const styles = StyleSheet.create({
         borderColor: '#CDCDCD',
         width: width * 0.7,
         height: height * 0.15,
+
         // backgroundColor:'red',
         marginRight: 5,
         borderRadius: 15,
@@ -165,17 +234,48 @@ const styles = StyleSheet.create({
         // justifyContent:'center',
         alignItems: 'center',
         gap: 5,
-        
+
 
 
     },
     productInfoContainer: {
-      
+
         borderColor: 'grey',
         width: width * 0.4,
         height: height * 0.14,
         justifyContent: 'center'
 
+    },
+    billingContainer: {
+        width: width * 0.91,
+        backgroundColor: '#E7E5DF',
+        // height:height*0.5,
+        borderRadius: 20,
+        marginBottom: 20,
+        // justifyContent:'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        marginTop: height * 0.015,
+        
+    },
+    divider: {
+        height: 2.5,
+        width: width * 0.84,
+        backgroundColor: '#433eb6',
+        opacity: 0.6
+
+    },
+    paymentStatus: {
+        width: width * 0.8,
+        height: height * 0.07,
+    //    backgroundColor:'#2FBF71',
+        borderRadius: 20,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop:height*0.02,
+        gap:width*0.03,
+        elevation:5
     }
 })
 
