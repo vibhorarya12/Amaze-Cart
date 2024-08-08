5f883350bd251caf4cba2878dac02f033238d398
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-08-08 21:14:02 +0530

    new ui for logged in user

--- app/screens/Profile/index.tsx
+++ app/screens/Profile/index.tsx
@@ -1,8 +1,8 @@
-import { ScrollView, StyleSheet, Dimensions , Image} from "react-native";
+import { ScrollView, StyleSheet, Dimensions, Image, TouchableOpacity, Alert } from "react-native";
 import { View, Text } from "react-native-animatable"
 import { removeItem } from "../../Utils/utils";
 import { LinearGradient } from "expo-linear-gradient";
-import { Avatar } from "react-native-paper";
+import { Avatar, Icon, IconButton } from "react-native-paper";
 import { Avatar_Img, Login_img } from "../../../assets/Images";
 import { Button } from "react-native-paper";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,56 +10,104 @@ import { logout } from "../../../redux/Actions/authActions";
 import { useEffect } from "react";
 
 const { width, height } = Dimensions.get('window');
-const color = [ '#E7E5DF' , '#E7E5DF'];
+const color = ['#E7E5DF', '#E7E5DF'];
 
 const Profile = ({ navigation }) => {
     const dispatch = useDispatch();
-  
-    const {token , loading , guestLogin , name}  = useSelector((state) => state.auth);
 
-    useEffect(()=>{
-        if(token.length===0){
+    const { token, loading, guestLogin, name } = useSelector((state) => state.auth);
+    const userInfo = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        if (token.length === 0) {
             alert('please login first');
-         }
+        }
 
 
-    },[token])
-     
+    }, [token])
 
+    const handleLogout = () => {
+        Alert.alert(
+            "", 
+            "Are you sure you want to logout?",
+            [
+                {
+                    text: "Cancel",
+                    onPress: () => console.log("Logout cancelled"),
+                    style: "cancel", 
+                },
+                {
+                    text: "OK",
+                    onPress: () => {
+                        dispatch(logout());
+                    },
+                },
+            ],
+            { cancelable: false } 
+        );
+    };
     return (<ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
 
- 
-        {token.length===0?<View style={[styles.profileContainer, { borderWidth: 0.4, borderColor: 'black', justifyContent: 'center' , gap:height*0.03}]}>
-            <Button onPressIn={()=>dispatch(logout())}>reset</Button>
-            <Image style={{height:height*0.35, width:width*0.55}}  source ={Login_img} />
+
+        {token.length === 0 ? <View style={[styles.profileContainer, { backgroundColor: '#F4F4F4',  justifyContent: 'center', gap: height * 0.03 }]}>
+            {/* <Button onPressIn={()=>dispatch(logout())}>reset</Button> */}
+            <Image style={{ height: height * 0.35, width: width * 0.55 }} source={Login_img} />
             <Text style={{
                 fontSize: width * 0.06,
                 fontFamily: 'RobotoSlab_semiBold', color: '#433eb6'
             }}>
                 Your are not logged in !!
             </Text>
-           
-            <Button onPress={() => navigation.navigate('AuthNav')} style={[styles.Btn,{top: 0 }]} icon="logout" mode="contained" >
-              Login now
+
+            <Button onPress={() => navigation.navigate('AuthNav')} style={[styles.Btn, { top: 0 }]} icon="logout" mode="contained" >
+                Login now
             </Button>
-            
-
-        </View>: <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} colors={color} style={styles.profileContainer}>
-            <Avatar.Image style={styles.avatar} size={width * 0.27} source={Avatar_Img} />
-            <Text style={styles.nameText}>{`Hey ${name} !`}</Text>
-            <Button  style={styles.Btn} icon="truck-fast-outline" mode="contained" onPress={() => navigation.navigate('MyOrders')}>
-                My orders
-            </Button>
-            <Button onPressIn={()=>dispatch(logout())} style={[styles.Btn,{top: height * 0.13 }]} icon="logout" mode="contained" onPress={() => removeItem("onboarded")}>
-               Log out
-            </Button>
-        </LinearGradient> }
-        
-
-        
 
 
+        </View> : <View style={styles.profileContainer}>
+            <Avatar.Image style={styles.avatar} size={width * 0.3} source={Avatar_Img} />
+            <Text style={styles.nameText}>{userInfo.name}</Text>
+            <View style={styles.infoItems}>
+                <View style={{ left: width * 0.04 }}>
+                    <Icon source={'account'} size={width * 0.07} color="#433eb6" />
+                </View>
+                <Text style={styles.infoText}>{userInfo.name}</Text>
+            </View>
+            <View style={styles.infoItems}>
+                <View style={{ left: width * 0.04 }}>
+                    <Icon source={'email'} size={width * 0.07} color="#433eb6" />
+                </View>
+                <Text style={styles.infoText}>{userInfo.email}</Text>
+            </View>
+            <View style={styles.infoItems}>
+                <View style={{ left: width * 0.04 }}>
+                    <Icon source={'phone'} size={width * 0.07} color="#433eb6" />
+                </View>
+                <Text style={styles.infoText}>{'+91 ' + userInfo.phone}</Text>
+            </View>
+            <View style={styles.infoItems}>
+                <View style={{ left: width * 0.04 }}>
+                    <Icon source={'google-maps'} size={width * 0.07} color="#433eb6" />
+                </View>
+                <Text style={styles.infoText}>{'123 Maple StreetSpring'}</Text>
+                <IconButton
+                    icon="square-edit-outline"
+                    iconColor='#433eb6'
+                    size={width * 0.07}
+                    onPress={()=>console.log(userInfo)}
+                    style={{right:width*0.02}}
+                />
 
+            </View>
+            <TouchableOpacity onPress={handleLogout} activeOpacity={0.6} style={styles.infoItems}>
+                <View style={{ left: width * 0.04 }}>
+                    <Icon source={'logout'} size={width * 0.07} color="#433eb6" />
+                </View>
+                <Text style={styles.infoText}>{'Logout'}</Text>
+            </TouchableOpacity>
+
+
+        </View>}
 
     </ScrollView>)
 }
@@ -72,31 +120,34 @@ const styles = StyleSheet.create({
     },
     contentContainer: {
         paddingTop: height * 0.2,
-        justifyContent: 'center',
+        // justifyContent: 'center',
         alignItems: 'center',
+        paddingBottom: 10
 
     },
     profileContainer: {
-        width: width * 0.89,
-        height: height * 0.6,
-        // borderWidth:2,
+        width: width * 0.95,
+        height: 'auto',
+        // borderWidth:1,
         // borderColor:'#433eb6',
-        borderRadius: 25,
-
+        borderRadius: 1,
+        gap: height * 0.025,
         alignItems: 'center'
 
     },
     avatar: {
-        position: 'absolute',
-        top: -height * 0.05,
-        zIndex: 2,
-        alignSelf: 'center',
+        // position: 'absolute',
+        // top: -height * 0.05,
+        // zIndex: 2,
+        // alignSelf: 'center',
+       elevation:5
 
     },
     nameText: {
-        top: height * 0.1,
+
         fontSize: width * 0.05,
-        fontFamily: 'RobotoSlab_semiBold'
+        fontFamily: 'RobotoSlab_semiBold',
+        color: 'black'
     },
     Btn: {
         backgroundColor: '#433eb6',
@@ -104,6 +155,22 @@ const styles = StyleSheet.create({
         width: width * 0.5,
         height: height * 0.06,
         elevation: 5
+    },
+    infoItems: {
+        width: width * 0.88,
+        height: height * 0.068,
+        elevation: 4,
+        borderColor: 'black',
+        borderRadius: 10,
+        backgroundColor: '#F4F4F4',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: width * 0.1
+    },
+    infoText: {
+        fontSize: width * 0.04,
+        fontFamily: 'RobotoSlab_regular',
+        color: 'black'
     }
 })
 
